80c279001fb123d87f4026d977b3f6c2f1b727da
David Seow <david.seow@theculturetrip.com>
2021-01-05 10:18:13 +0000

    Enable Cypress component testing

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,6 +16,9 @@
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+  require("cypress-react-unit-test/plugins/react-scripts")(on, config);
+
+  // IMPORTANT to return the config object
+  // with the any changed environment variables
+  return config;
+};
